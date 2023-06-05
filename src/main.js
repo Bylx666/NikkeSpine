dacfefc8ead015d60d2f12c9743d6e5ae3bec759
Subkey <47173414+Bylx666@users.noreply.github.com>
2023-06-05 20:56:13 +0800

    cdn original

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var load = function (
 	currentSkeleton = 'c010',
 	currentPosture = 'stand',
 	currentAnimation = 'idle',
-	cdn = 'https://cdn.jsdelivr.net/gh/Pelom777/NikkeSpine/'
+	cdn = '/'
 ) {
 	var isMobile;
 	var lastFrameTime = Date.now() / 1000;
